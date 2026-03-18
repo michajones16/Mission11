d1092dd1162733af788b53ae53637b9f4cec526d
michajones16 <michajones16@gmail.com>
2026-03-18 10:18:28 -0600

    Initial Commit

---
+++ backend/Mission11.API/Data/Book.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mission11.API.Data
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Book
+    {
+        [Key]
+        public int BookID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        public string ISBN { get; set; }
+        public string Classification { get; set; }
+        public string Category { get; set; }
+        public int PageCount { get; set; }
+        public float Price { get; set; }
+    }
+}
